e11e7c99c9f6d9277a0fac568b3543370c048939
The_Android_Penguin <49669409+AndroidPenguin@users.noreply.github.com>
2020-05-30 10:48:22 +1200

    Comic navigation
    - Added JavaScript file with arrary of comic attributes.
    
    - Created functions to index between comics using navigation buttons.
    
    - Changed text beneath comic to welcome message.
    
    - Uploaded first 4 comics.

---
+++ comic_list.js
@@ -0,0 +1,13 @@
+var comic_list = [
+  {"filepath":"comics/comic_01.png", "title":"Decapitated Stuff",
+    "hoverText":"Anyone down for rabbit and frog stew?"},
+
+  {"filepath":"comics/comic_02.png", "title":"Peacharine",
+    "hoverText":"For context, the last frame is a typo"},
+
+  {"filepath":"comics/comic_03.png", "title":"Outside Your Window",
+    "hoverText":"Who cares about the house, I'm more intrested in that lamp"},
+
+  {"filepath":"comics/comic_04.png", "title":"Iced Animals",
+    "hoverText":"I imagined them as animal shaped ice blocks"}
+]
