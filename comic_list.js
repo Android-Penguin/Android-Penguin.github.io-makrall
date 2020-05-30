f36b91cc15b67a38ecf28d6b7d5594d854d94fd6
The_Android_Penguin <49669409+AndroidPenguin@users.noreply.github.com>
2020-05-30 16:47:48 +1200

    Archive page
    - Made archive page with dynamically created list of comics.
    
    - Added code to initialize function on index page to read search parameters from archive page and open selected comic.
    
    - Added date label to comic_list array.
    
    - Changed mouse pointer type when hovering over comic nav button.

--- comic_list.js
+++ comic_list.js
@@ -1,13 +1,13 @@
 var comic_list = [
-  {"filepath":"comics/comic_01.png", "title":"Decapitated Stuff",
+  {"filepath":"comics/comic_01.png", "title":"Decapitated Stuff","date":"17/04/20",
     "hoverText":"Anyone down for rabbit and frog stew?"},
 
-  {"filepath":"comics/comic_02.png", "title":"Peacharine",
-    "hoverText":"For context, the last frame is a typo"},
+  {"filepath":"comics/comic_02.png", "title":"Peacharine", "date":"17/04/20",
+    "hoverText":"Yum"},
 
-  {"filepath":"comics/comic_03.png", "title":"Outside Your Window",
+  {"filepath":"comics/comic_03.png", "title":"Outside Your Window", "date":"17/04/20",
     "hoverText":"Who cares about the house, I'm more intrested in that lamp"},
 
-  {"filepath":"comics/comic_04.png", "title":"Iced Animals",
+  {"filepath":"comics/comic_04.png", "title":"Iced Animals", "date":"17/04/20",
     "hoverText":"I imagined them as animal shaped ice blocks"}
 ]
