52498770c4c383b275babbacf9673781aaf0981b
The_Android_Penguin <49669409+AndroidPenguin@users.noreply.github.com>
2020-05-31 21:18:33 +1200

    Image viewer modal box
    - Added modal box to view enlarged version of image when image is clicked.
    
    - Changed album_list array to pass file name instead of ID for onclick function.
    
    - Removed extra transparancy from non-square images.
    
    - Added "Grass Leaf" image.

--- album_list.js
+++ album_list.js
@@ -1,22 +1,25 @@
 var album_list = [
   {"filepath":"we_dem_kois/01_bath_oat.png", "name":"Bath Oat",
-    "id":"album01", "onclick":"modalOpen(album01)"},
+    "id":"album01", "onclick":"modalOpen('we_dem_kois/01_bath_oat.png')"},
 
   {"filepath":"we_dem_kois/02_banjo_ice.png", "name":"Banjo Ice",
-    "id":"album02", "onclick":"modalOpen(album02)"},
+    "id":"album02", "onclick":"modalOpen('we_dem_kois/02_banjo_ice.png')"},
 
   {"filepath":"we_dem_kois/03_plant_tube.png", "name":"Plant Tube",
-    "id":"album03", "onclick":"modalOpen(album03)"},
+    "id":"album03", "onclick":"modalOpen('we_dem_kois/03_plant_tube.png')"},
 
   {"filepath":"we_dem_kois/04_lemon_knife.png", "name":"Lemon Knife",
-    "id":"album04", "onclick":"modalOpen(album04)"},
+    "id":"album04", "onclick":"modalOpen('we_dem_kois/04_lemon_knife.png')"},
 
   {"filepath":"we_dem_kois/05_melon_river.png", "name":"Melon River",
-    "id":"album05", "onclick":"modalOpen(album05)"},
+    "id":"album05", "onclick":"modalOpen('we_dem_kois/05_melon_river.png')"},
 
   {"filepath":"we_dem_kois/06_chisel_wire.png", "name":"Chisel Wire",
-    "id":"album06", "onclick":"modalOpen(album06)"},
+    "id":"album06", "onclick":"modalOpen('we_dem_kois/06_chisel_wire.png')"},
 
   {"filepath":"we_dem_kois/07_rice_lamp.png", "name":"Rice Lamp",
-    "id":"album07", "onclick":"modalOpen(album07)"},
+    "id":"album07", "onclick":"modalOpen('we_dem_kois/07_rice_lamp.png')"},
+
+  {"filepath":"we_dem_kois/08_glass_leaf.png", "name":"Glass Leaf",
+    "id":"album080", "onclick":"modalOpen('we_dem_kois/08_glass_leaf.png')"}
 ];
