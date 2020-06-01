b87bd3dbb416e2399563c8faf2323bea9ea696d5
The_Android_Penguin <49669409+AndroidPenguin@users.noreply.github.com>
2020-06-01 12:17:36 +1200

    "Digital Art" project completed
    - Uploaded art images.
    
    - Added image data for art list JS file.
    
    - Wrote script to add images into the project 3 <div>.
    
    - Adjusted modal padding.

--- art_list.js
+++ art_list.js
@@ -1,3 +1,19 @@
 var art_list = [
-  {},
+  {"filepath":"digital_art/art03.png", "name":"Alternate Dimension",
+    "id":"art03", "onclick":"modalOpen('digital_art/art03.png')"},
+
+  {"filepath":"digital_art/art04.png", "name":"Mystical Forest",
+    "id":"art04", "onclick":"modalOpen('digital_art/art04.png')"},
+
+  {"filepath":"digital_art/art05.png", "name":"Destructive Snowman",
+    "id":"art05", "onclick":"modalOpen('digital_art/art05.png')"},
+
+  {"filepath":"digital_art/art06.png", "name":"Burning Sea",
+    "id":"art06", "onclick":"modalOpen('digital_art/art06.png')"},
+
+  {"filepath":"digital_art/art07.png", "name":"Cave of Chaos",
+    "id":"art07", "onclick":"modalOpen('digital_art/art07.png')"},
+
+  {"filepath":"digital_art/art08.png", "name":"Lost Quality",
+    "id":"art08", "onclick":"modalOpen('digital_art/art08.png')"},
 ];
